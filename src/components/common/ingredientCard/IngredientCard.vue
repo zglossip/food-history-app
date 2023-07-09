a5f4151fa2ab7cc43de0bccc80901ebc28c1a931
Zach Glossip <zglossip@gmail.com>
2023-07-09 17:02:34 -0500

    Start ingredient card (WIP)

--- src/components/common/ingredientCard/IngredientCard.vue
+++ src/components/common/ingredientCard/IngredientCard.vue
@@ -1,1 +1,25 @@
-<template><div /></template>
+<template>
+    <ion-card>
+        <ion-card-content>
+            <ion-list>
+                <ion-item v-for="ingredient in ingredients">
+                
+                </ion-item>
+            </ion-list>
+        </ion-card-content>
+    </ion-card>
+</template>
+
+<script setup lang="ts">
+import { useIngredientCardService , injectionKey} from '@/components/common/ingredientCard/ingredientCardService';
+import { inject } from 'vue';
+import { IonCard, IonCardContent, IonList, IonItem } from '@ionic/vue';
+
+interface Props {
+    recipeId: number
+}
+
+const props = defineProps<Props>()
+
+const {isLoading, ingredients} = inject(injectionKey, useIngredientCardService)(props.recipeId)
+</script>
