fc3083c927c70f83302936b26e8a71248cc8f66a
Zach Glossip <zglossip@gmail.com>
2025-09-13 13:23:59 -0500

    Move URL to environment variable

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -2,7 +2,7 @@ import { FilterType } from "@/types/FilterType";
 
 //TODO: Add something in the readme about this
 //TODO: Make this environment specific ESPECIALLY BEFORE DEPLOY
-export const BACKEND_BASE = "http://localhost:8080";
+export const BACKEND_BASE = import.meta.env.BACKEND_BASE;
 
 export const FILTER_OPTIONS: FilterType[] = [
   FilterType.COURSE,
