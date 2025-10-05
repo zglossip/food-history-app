dc6feff181d7aab352d6981eb773291bb431b2b4
Zach Glossip <zglossip@gmail.com>
2025-10-05 16:07:58 -0500

    Remove menu and fix router bug

--- src/components/common/basePage/BasePage.vue
+++ src/components/common/basePage/BasePage.vue
@@ -4,7 +4,6 @@ import {
   IonButtons,
   IonButton,
   IonIcon,
-  IonMenu,
   IonToolbar,
   IonTitle,
   IonContent,
@@ -12,44 +11,22 @@ import {
   IonMenuButton,
 } from "@ionic/vue";
 import { home } from "ionicons/icons";
-import { useSlots } from "vue";
-import { useRouter } from "vue-router";
 
 //PROPS
 
 interface Props {
   title: string;
-  menuTitle?: string;
 }
 
-const { title, menuTitle = "Menu" } = defineProps<Props>();
-
-const slots = useSlots();
-const router = useRouter();
-
-const goHome = (): void => {
-  router.push("/");
-};
+defineProps<Props>();
 </script>
 
 <template>
-  <ion-menu v-if="!!slots.menu" content-id="main-content" side="end">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>
-          {{ menuTitle }}
-        </ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-      <slot name="menu" />
-    </ion-content>
-  </ion-menu>
   <ion-page content-id="main-content">
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-buttons slot="start">
-          <ion-button @click="goHome">
+          <ion-button router-link="/">
             <ion-icon slot="icon-only" :icon="home" />
           </ion-button>
         </ion-buttons>
--- src/views/browseView/BrowseView.vue
+++ src/views/browseView/BrowseView.vue
@@ -3,22 +3,49 @@ import RecipeItem from "@/components/browse/recipeItem/RecipeItem.vue";
 import FilterMenu from "@/components/browse/filterMenu/FilterMenu.vue";
 import BasePage from "@/components/common/basePage/BasePage.vue";
 import { useBrowseViewService } from "./browseViewService";
+import { filterCircleOutline } from "ionicons/icons";
+import {
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonModal,
+  IonContent,
+} from "@ionic/vue";
+import { ref } from "vue";
+import { Filters } from "@/components/browse/filterMenu/filterMenuService";
 
 const { recipes, name, courses, cuisines, tags, applyFilters } =
   useBrowseViewService();
+
+const modalOpen = ref(false);
+
+const openModal = () => (modalOpen.value = true);
+const closeModal = () => (modalOpen.value = false);
+
+const onApply = (filters: Filters) => {
+  applyFilters(filters);
+  closeModal();
+};
 </script>
 
 <template>
-  <BasePage title="Browse" menu-title="Filter">
-    <template #menu>
-      <filter-menu
-        :starting-name="name"
-        :starting-course-types="courses"
-        :starting-cuisine-types="cuisines"
-        :starting-tags="tags"
-        @apply="applyFilters"
-      />
-    </template>
+  <BasePage title="Browse">
     <recipe-item v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+    <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+      <ion-fab-button @click="openModal">
+        <ion-icon :icon="filterCircleOutline" />
+      </ion-fab-button>
+    </ion-fab>
+    <ion-modal :is-open="modalOpen" @did-dismiss="closeModal">
+      <ion-content class="ion-padding">
+        <filter-menu
+          :starting-name="name"
+          :starting-course-types="courses"
+          :starting-cuisine-types="cuisines"
+          :starting-tags="tags"
+          @apply="onApply"
+        />
+      </ion-content>
+    </ion-modal>
   </BasePage>
 </template>
